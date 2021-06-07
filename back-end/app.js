674d042daaa37d448a666ea7465b8aeb01142afb
levshpilko <levs101@gmail.com>
2021-06-07 17:30:16 +0300

    added process.env.PORT to app,js in server

--- back-end/app.js
+++ back-end/app.js
@@ -60,6 +60,6 @@ mongoose
 	)
 	.then(() => {
 		console.log('******************DB CONNECTED!******************');
-		app.listen(5000);
+		app.listen(process.env.PORT || 5000);
 	})
 	.catch(err => console.log(err));
